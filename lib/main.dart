f2af636c8e15904e7f7ecea66014af846f7902a2
Dinesh <dkofficial24@gmail.com>
2024-06-07 21:21:05 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+void main() {
+  double weight = 70; // in kilograms
+  double height = 1.625; // in meters
+
+  double bmi = weight / (height * height);
+   print(bmi);
+  if (bmi < 18.5) {
+    print('Underweight');
+  } else if (bmi >= 18.5 && bmi < 24.9) {
+    print('Normal weight');
+  } else if (bmi >= 25.0 && bmi < 29.9) {
+    print('Overweight');
+  } else {
+    print('Obesity');
+  }
+}
