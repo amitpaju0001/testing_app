eb18c78011bd00e1f30db88187c8889342f00b25
Amit <dkofficial24@gmail.com>
2024-06-07 21:31:09 +0530

    - minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
   if (bmi < 18.5) {
     print('Underweight');
   } else if (bmi >= 18.5 && bmi < 24.9) {
-    print('Normal weight');
+    print('Normal weight..');
   } else if (bmi >= 25.0 && bmi < 29.9) {
     print('Overweight');
   } else {
